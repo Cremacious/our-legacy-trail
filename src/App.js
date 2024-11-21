b479f28401f229dbfad8f37022aa711f8c77b0ac
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-11-21 18:26:29 -0500

    The Strava API now successfully fetches athlete data.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import Home from './routes/home/home.component';
 import { Buffer } from 'buffer';
-import { getActivities } from './utils/strava.utils';
+import { getAthlete } from './utils/strava.utils';
 
 global.Buffer = Buffer;
 
 function App() {
-  const getStravaActivities = async () => {
-    getActivities();
-  };
+  // const getStravaActivities = async () => {
+  //   getActivities();
+  // };
 
   return (
     <div className="App">
       <header className="App-header">
         <Home />
-        <button onClick={getStravaActivities}>Get Strava Activities</button>
+        <button onClick={getAthlete}>Get Strava Activities</button>
       </header>
     </div>
   );
--- src/utils/strava.utils.js
+++ src/utils/strava.utils.js
@@ -1,20 +1,10 @@
 import strava from 'strava-v3';
 
-const CLIENT_ID = '139244';
-const CLIENT_SECRET = 'd40128b97225afafe8fe1ed0b8243a04cb6f4562';
-const ACCESS_TOKEN = 'd3b3f849377908120d61a3b183f21a6fd3ce969f';
+const access_token = 'f08b4b5383b6f2eda750b1e0d1803eec8fb46321';
 
-strava.config({
-  access_token: ACCESS_TOKEN,
-  client_id: CLIENT_ID,
-  client_secret: CLIENT_SECRET,
-  redirect_uri: 'http://localhost:3000',
-});
-// const payload = await strava.athlete.get({});
-// console.log(payload);
-
-export const getActivities = async () => {
-  // const payload = await strava.athlete.listActivities({});
-  // console.log(payload);
-  // return payload;
-}
+export const getAthlete = async () => {
+    const userStrava = new strava.client(access_token);
+    const payload = await userStrava.athlete.get({});
+    console.log(payload);
+  };
+  
