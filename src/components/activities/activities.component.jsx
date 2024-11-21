fd389d5d8f4264e48611a76b92d831aa41ebe599
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-11-20 21:20:21 -0500

    Cleaned up missing files.

--- src/components/activities/activities.component.jsx
+++ src/components/activities/activities.component.jsx
@@ -1,31 +1,11 @@
 import { useState, useEffect } from 'react';
 
 import Activity from '../activity/activity.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
-import { fetchActivityData, refreshToken } from '../../utils/strava.utils';
-=======
-import { fetchActivityData, requestAccess } from '../../utils/strava.utils';
->>>>>>> parent of 91e667d (Possible fix)
-// import { tempData } from '../../utils/temp-data';
->>>>>>> parent of a31c9f7 (Added a function for redirecting for auth.)
 
 function Activities() {
   const [activities, setActivities] = useState([]);
 
-  useEffect(() => {
 
-  }, []);
-
-  const fetchActivity = async () => {
-    // const data = await fetchActivityData();
-    // console.log('Data:', data);
-    // if (data) {
-    //   setActivities(data);
-  };
 
   return (
     <div>
@@ -33,16 +13,6 @@ function Activities() {
       {activities.map((activity) => (
         <Activity key={activity.id} activity={activity} />
       ))}
-      <button onClick={fetchActivity}>Fetch Data</button>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      <button onClick={refreshToken}>Refresh Token</button>
->>>>>>> parent of a31c9f7 (Added a function for redirecting for auth.)
-=======
-      {/* <button onClick={refreshToken}>Refresh Token</button> */}
-      <button onClick={requestAccess}>Request Access</button>
->>>>>>> parent of 91e667d (Possible fix)
     </div>
   );
 }
