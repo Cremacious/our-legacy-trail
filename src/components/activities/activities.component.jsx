9f5b726c6269c118c66f66147e808690743c4d10
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-11-20 20:29:04 -0500

    Revert "Added a function for redirecting for auth."
    This reverts commit a31c9f7c8758f8ba2085dda2d570a766e0b4bd26.

--- src/components/activities/activities.component.jsx
+++ src/components/activities/activities.component.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react';
 
 import Activity from '../activity/activity.component';
+<<<<<<< HEAD
 
 
+=======
+import { fetchActivityData, refreshToken } from '../../utils/strava.utils';
+// import { tempData } from '../../utils/temp-data';
+>>>>>>> parent of a31c9f7 (Added a function for redirecting for auth.)
 
 function Activities() {
   const [activities, setActivities] = useState([]);
@@ -25,6 +30,10 @@ function Activities() {
         <Activity key={activity.id} activity={activity} />
       ))}
       <button onClick={fetchActivity}>Fetch Data</button>
+<<<<<<< HEAD
+=======
+      <button onClick={refreshToken}>Refresh Token</button>
+>>>>>>> parent of a31c9f7 (Added a function for redirecting for auth.)
     </div>
   );
 }
--- src/utils/strava.utils.js
+++ src/utils/strava.utils.js
@@ -4,6 +4,7 @@ const CLIENT_ID = '139244';
 const CLIENT_SECRET = 'd40128b97225afafe8fe1ed0b8243a04cb6f4562';
 const ACCESS_TOKEN = 'd3b3f849377908120d61a3b183f21a6fd3ce969f';
 
+<<<<<<< HEAD
 strava.config({
   access_token: ACCESS_TOKEN,
   client_id: CLIENT_ID,
@@ -16,3 +17,58 @@ export const fetchData = async () => {
   // console.log(payload);
   console.log('Fetching data...');
 };
+=======
+export async function refreshToken() {
+  try {
+    const response = await fetch('https://www.strava.com/api/v3/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: REFRESH_TOKEN,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log('Token:', data.access_token);
+    ACCESS_TOKEN = data.access_token;
+    return data.access_token;
+  } catch (error) {
+    console.error('Error refreshing token:', error);
+  }
+}
+
+export async function fetchActivityData() {
+  try {
+    if (!ACCESS_TOKEN) {
+      await refreshToken();
+    }
+    const response = await fetch(
+      'https://www.strava.com/api/v3/athlete/activities?per_page=30',
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error!!!! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log('Athlete Activities:', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching athlete stats:', error);
+  }
+}
+>>>>>>> parent of a31c9f7 (Added a function for redirecting for auth.)
